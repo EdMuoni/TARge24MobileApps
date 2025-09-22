8d5d87ce3d4fd45e1c1941fcdf02a2c6d6e18891
Edgar <edgar.muoni@gmail.com>
2025-09-22 16:48:49 +0300

    PlanetsPage

--- MauiPlanets/MauiPlanets/Views/PlanetsPage.xaml.cs
+++ MauiPlanets/MauiPlanets/Views/PlanetsPage.xaml.cs
@@ -1,4 +1,8 @@
 using MauiPlanets.Services;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using System;
+using MauiPlanets.Models;
 
 namespace MauiPlanets.Views;
 
@@ -17,5 +21,20 @@ public partial class PlanetsPage : ContentPage
 
 		lstPopularPlanets.ItemSource = PlanetsService.GetFeaturedPlanets();
 		lstAllPlanets.ItemSource = PlanetsService.GetAllPlanets();
-    }
+	}
+
+	async void Planets_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
+	{
+		//await Navigation.PushAsync(new PlanetsDetailsPage(e. CurrentSelection.First() as Planet));
+	}
+
+        async void ProfilePic_Clicked(System.Object sender, System.EventArgs e)
+	{
+		_ = MainContentGrid.TranslateTo(-this.Width * 0.5, this.Height * 0.1, AnimationDuration, Easing.CubicIn);
+
+		await MainContentGrid.ScaleTo(0.8, AnimationDuration);
+		_ = MainContentGrid.ScaleTo(0.8, AnimationDuration);
+	}
+
+   
 }
